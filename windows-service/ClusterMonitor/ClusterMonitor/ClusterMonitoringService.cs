71b7b163a25c69312de267d20f986a33e5a85c2b
Deep Kapur <dekapur@microsoft.com>
2018-03-26 18:57:18 -0700

    two apps

---
+++ windows-service/ClusterMonitor/ClusterMonitor/ClusterMonitoringService.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.Diagnostics.EventFlow;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Linq;
+using System.ServiceProcess;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClusterMonitor
+{
+    public partial class clusterMonitoringService : ServiceBase
+    {
+        public clusterMonitoringService()
+        {
+            InitializeComponent();
+        }
+
+        protected override void OnStart(string[] args)
+        {
+            using (var pipeline = DiagnosticPipelineFactory.CreatePipeline("eventFlowConfig.json"))
+            {
+                System.Diagnostics.Trace.TraceWarning("EventFlow is working!");
+                Console.ReadLine();
+            }
+        }
+
+        protected override void OnStop()
+        {
+            //TODO: Implement shutdown logic? 
+        }
+    }
+}
